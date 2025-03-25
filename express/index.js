67a4bc016042d863430a6fd0e5ac246b23160974
dipangshu22 <dipangshu22@gmail.com>
2025-03-25 21:56:52 +0530

    okay

--- express/index.js
+++ express/index.js
@@ -1,8 +1,21 @@
 const express=require("express")
 const app=express()
+const fs=require("fs")
 
 app.get('/',(req,res)=>{
     res.send("hello world")
+
+})
+
+
+app.get('/movies',(req,res)=>{
+    fs.readFile('./movie.json',(err,result)=>{
+        if(err)
+            console.log("error")
+        else{
+            res.send(JSON.parse(result))
+        }
+    })
 
 })
 
